06a49d36bf9a557a2135266f236e7f02be509e12
BT_1N3_28 <ctam9591@bths.edu>
2018-12-11 11:42:16 -0500

    update

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 
 //Challenge Two: Deck Class
 public class Deck
@@ -8,9 +9,16 @@ public class Deck
     ArrayList<Card> unDealt;
     ArrayList<Card> Dealt;
 
-    public Deck(String[] rank, String[] suits, String[] pointValue)
+    public Deck(String[] rank, String[] suit, int[] pointValue)
     {
-        ArrayList<Card> deck = new ArrayList<Card>();
+        List<Card> deck = new ArrayList<Card>();
 
+        for(int i = 0; i < rank.length; i++)
+        {
+            for(int j = 0; i < suit.length; i++)
+            {
+                deck.add(new Card(rank[j], suit[i], pointValue[j]));
+            }
+        }
     }
 }
