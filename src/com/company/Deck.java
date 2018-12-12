58d5b5220a39c4a6055952eca7565ca120cf0d5f
BT_1N3_28 <ctam9591@bths.edu>
2018-12-12 11:44:00 -0500

    update

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -21,4 +21,42 @@ public class Deck
             }
         }
     }
+
+    public boolean isEmpty()
+    {
+        return unDealt.size() <= 0;
+    }
+
+    public int size(int amount)
+    {
+        return unDealt.size();
+    }
+
+    public Card deal()
+    {
+        if(this.unDealt.size() <= 0)
+        {
+            return null;
+        }
+        Card topCard = this.unDealt.remove(0);
+        this.Dealt.add(topCard);
+        return topCard;
+    }
+
+    public void shuffle()
+    {
+        System.out.println(this.unDealt);
+        for(int i = 0; i < this.Dealt.size()-1; i++)
+        {
+            this.unDealt.add(this.Dealt.get(i));
+        }
+        for(int k = this.unDealt.size()-1; k > 0; k--)
+        {
+            int r = (int)(Math.random() * this.unDealt.size());
+            Card temp = this.unDealt.get(k);
+            this.unDealt.set(k, this.unDealt.get(r));
+            this.unDealt.set(r, temp);
+        }
+
+    }
 }
